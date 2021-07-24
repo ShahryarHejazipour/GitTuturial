1df84ea1fb13ad9800c399919d629152d867cea5
Shahryar Hejazipour <shahryar.hejazipour@outlook.com>
2021-07-24 12:45:41 +0430

    first commit

---
+++ app/src/main/java/com/tispunshahryar960103/gittutorial/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tispunshahryar960103.gittutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
